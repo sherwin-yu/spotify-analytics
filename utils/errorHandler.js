c9b0d64d4c38c1411b4538f2d388a1809c3ae604
Sherwin Yu <sherwin.yu@us.ibm.com>
2019-08-21 00:26:26 -0400

    add errorhandler

---
+++ utils/errorHandler.js
@@ -0,0 +1,13 @@
+require('dotenv').config();
+
+module.exports = (err, location, res) => {
+  // eslint-disable-next-line no-console
+  console.error(`
+    -------------------------
+    Function Name: ${location}
+    Error Name: ${err.name}
+    Error Message: ${err.message}
+    Error Stack: ${err.stack}
+    -------------------------`);
+  res.status(err.status).json(err.message);
+};
