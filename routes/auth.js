b8c38e94ffad4eb6a7d325fc2b0dbbbbbce7a14e
Sherwin Yu <sherwinhyu@gmail.com>
2019-08-22 02:20:12 -0400

    cleanup

--- routes/auth.js
+++ routes/auth.js
@@ -6,7 +6,6 @@ const { CLIENT_ID, REDIRECT_URI } = process.env;
 const router = express.Router();
 
 const spotifyLogin = (req, res) => {
-  // your application requests authorization
   const scopes = 'user-read-private user-read-email';
   res.redirect(
     `https://accounts.spotify.com/authorize?response_type=code&client_id=${CLIENT_ID}&scope=${encodeURIComponent(
