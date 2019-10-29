b79ec475146fa9645411f3403192ab825820080b
Sherwin Yu <sherwinhyu@gmail.com>
2019-10-29 01:28:17 -0400

    add genre text

--- src/components/Home/TopGenres.js
+++ src/components/Home/TopGenres.js
@@ -11,38 +11,50 @@ const Title = styled.div`
   font-weight: 600;
 `;
 
+const Text = styled.div`
+  font-size: 28px;
+  align-self: center;
+`;
+
 const TopGenres = ({ genres }) => (
-  <div className="col-md-12">
+  <div>
     <Title>Top Genres</Title>
-    <HeightWrapper>
-      <ResponsivePie
-        data={genres.slice(0, 5)}
-        margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
-        innerRadius={0.5}
-        padAngle={0.7}
-        cornerRadius={3}
-        colors={{ scheme: 'yellow_green' }}
-        borderWidth={1}
-        borderColor={{ from: 'color', modifiers: [['darker', 0.2]] }}
-        enableRadialLabels={false}
-        radialLabelsSkipAngle={10}
-        radialLabelsTextXOffset={6}
-        radialLabelsTextColor="#333333"
-        radialLabelsLinkOffset={0}
-        radialLabelsLinkDiagonalLength={16}
-        radialLabelsLinkHorizontalLength={24}
-        radialLabelsLinkStrokeWidth={1}
-        radialLabelsLinkColor={{ from: 'color', modifiers: [] }}
-        sliceLabel="id"
-        slicesLabelsSkipAngle={10}
-        slicesLabelsTextColor="#333333"
-        sortByValue="true"
-        animate="true"
-        motionStiffness={90}
-        motionDamping={15}
-        isInteractive={false}
-      />
-    </HeightWrapper>
+    <div className="row">
+      <div className="col-md-5" style={{ display: 'flex' }}>
+        <Text>Your top genre is POP with it occuring 40% of the time in your top songs</Text>
+      </div>
+      <div className="col-md-7">
+        <HeightWrapper>
+          <ResponsivePie
+            data={genres.slice(0, 5)}
+            margin={{ top: 40, right: 80, bottom: 80, left: 80 }}
+            innerRadius={0.5}
+            padAngle={0.7}
+            cornerRadius={3}
+            colors={{ scheme: 'yellow_green' }}
+            borderWidth={1}
+            borderColor={{ from: 'color', modifiers: [['darker', 0.2]] }}
+            enableRadialLabels={false}
+            radialLabelsSkipAngle={10}
+            radialLabelsTextXOffset={6}
+            radialLabelsTextColor="#333333"
+            radialLabelsLinkOffset={0}
+            radialLabelsLinkDiagonalLength={16}
+            radialLabelsLinkHorizontalLength={24}
+            radialLabelsLinkStrokeWidth={1}
+            radialLabelsLinkColor={{ from: 'color', modifiers: [] }}
+            sliceLabel="id"
+            slicesLabelsSkipAngle={10}
+            slicesLabelsTextColor="#333333"
+            sortByValue="true"
+            animate="true"
+            motionStiffness={90}
+            motionDamping={15}
+            isInteractive={false}
+          />
+        </HeightWrapper>
+      </div>
+    </div>
   </div>
 );
 
